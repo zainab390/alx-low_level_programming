502e44c807f1641f2bf939823aea7f0175541036
Alhassan Zainab <azainab390gmail.com>
2023-02-21 20:45:29 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,35 @@
 #include "main.h"
-
 /**
- * main - check the code
+ * jack_bauer-prints all minutes in a day from 00:00
  *
- * Return: Always 0.
+ * Return:0 if succeesful.
  */
-int main(void)
+
+
+void jack_bauer(void)
 {
-	jack_bauer();
-	return (0);
+	int i, j, k, m;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					if (i == 2 && j > 3)
+					{break;
+					}
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(m + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
+
