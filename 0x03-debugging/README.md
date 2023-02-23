67af700e85b915451a52bf19ed77bf3897ff6bb6
Alhassan Zainab <azainab390gmail.com>
2023-02-23 14:32:04 -0800

    task 0

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+0. Multiple mains
+1.  Like, comment, subscribe
+2. This program prints the largest of three integers
+3. This program converts a date to the day of year and determines how many days are left in the year, taking leap year into consideration.
+
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * positive_or_negative - Entry point
+ * @i : integer
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+if (i < 0)
+{
+printf("%d is negative\n", i);
+}
+if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+}
