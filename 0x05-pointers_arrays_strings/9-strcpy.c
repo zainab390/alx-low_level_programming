35a7df0a56e5c8c01aec3d9204afcf44737fdd41
Alhassan Zainab <azainab390gmail.com>
2023-02-27 15:04:38 -0800

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,21 +1,16 @@
 #include "main.h"
-
 /**
- * _strcpy - copies the string pointed to by src
- * @dest: destination
- * @src: sourse
- * return: string1
+ * _strcpy - copy the string pointed to by src to dest
+ * @dest: char to check
+ * @src: char to check
+ * Return: 0 is success
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len = 0;
+	int a;
 
-	while (*(src + len) != '\0')
-	{
-		*(dest + len) = *(src + len);
-		len++;
-	}
-
-	*(dest + len) = '\0';
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
 	return (dest);
 }
