e399f15c78a554ea0eaa5797bbd270965f62c1c2
Alhassan Zainab <azainab390gmail.com>
2023-03-11 01:57:26 -0800

    task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
+
