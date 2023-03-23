704bb37609ef83ff6bf56fb80f123360ae771567
Alhassan Zainab <azainab390gmail.com>
2023-03-23 15:53:32 -0700

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,14 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+
+/**
+* struct format_struct - format-type class
+* @format: format identifier
+* @fmt_print_func: pointer to function that prints in the format of @format
+*/
+
+
 struct format_struct
 {
 	char format;
